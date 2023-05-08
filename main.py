2e8ad5f632d2baf980ceb407c0c038ce02104006
jes-2 <130179214+jes-2@users.noreply.github.com>
2023-05-08 01:55:14 -0500

    deleted main.py junk

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
 import db
 import decimal
-
-def modifybal(id,amt):
-    
-    db.connect()
-    bal = db.csr(f"SELECT balance FROM indentifying WHERE id = '{id}'")
-    db.csr(f"UPDATE indentifying SET balance = '{bal+amt}' WHERE id = '{id}'")
